f3a352f4659c14244733df0b523d08428e44cd19
Jainil Dadhaniya <jdadhaniya6@gmail.com>
2024-05-23 09:57:39 +0530

    Complete About Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import About from "./components/about/About";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 
@@ -9,6 +10,7 @@ function App() {
 
       <main className="main">
         <Home />
+        <About />
       </main>
     </>
   );
