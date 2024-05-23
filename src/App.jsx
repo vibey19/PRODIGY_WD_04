932aee4232d342ef8c7b268cb91e1758184b3660
Jainil Dadhaniya <jdadhaniya6@gmail.com>
2024-05-23 19:46:23 +0530

    Complete Portfolio Section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import About from "./components/about/About";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
+import Portfolio from "./components/projects/Portfolio";
 import Qualification from "./components/qualification/Qualification";
 import Services from "./components/services/Services";
 import Skills from "./components/skills/Skills";
@@ -17,6 +18,7 @@ function App() {
         <Skills />
         <Services />
         <Qualification />
+        <Portfolio />
       </main>
     </>
   );
