6fdaab04986ea88173c6e8bf300d221bb3f2873e
Jainil Dadhaniya <jdadhaniya6@gmail.com>
2024-06-18 18:36:28 +0530

    added final components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import About from "./components/about/About";
 import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import Portfolio from "./components/projects/Portfolio";
 import Qualification from "./components/qualification/Qualification";
+import ScrollUp from "./components/scrollup/ScrollUp";
 import Services from "./components/services/Services";
 import Skills from "./components/skills/Skills";
 import Testimonials from "./components/testimonials/Testimonials";
@@ -23,6 +25,8 @@ function App() {
         <Portfolio />
         <Testimonials />
         <Contact />
+        <ScrollUp />
+        <Footer />
       </main>
     </>
   );
